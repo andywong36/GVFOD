433d47729c2c5c83c8cf1b6c30dcafd8acb60437
leafloose <andy.wong@ualberta.ca>
2021-05-28 00:25:48 -0600

    fixed typo in hmm tuning options

--- exp/exp_gvfod/tuning_settings.py
+++ exp/exp_gvfod/tuning_settings.py
@@ -199,7 +199,7 @@ hmm_exp = Experiment(
     metric="f1",
     parameters={
         "n_sensors": 3,
-        "n_states": hyperopt.pyll.scope.int(hp.choice("n_states"), (4, 8, 16, 32)),
+        "n_states": hyperopt.pyll.scope.int(hp.choice("n_states", (4, 8, 16, 32))),
         "contamination": 0.05,
     },
     runs=8,
